f6a150fa9aec66baa776dd7910dc7167dc88e2af
Mohan Cm <mohanmanjappa@gmail.com>
2019-07-29 00:26:59 +0530

    Add a RecyclerView

--- app/src/main/java/com/mohan/roomwordsample/Model/Word.java
+++ app/src/main/java/com/mohan/roomwordsample/Model/Word.java
@@ -10,7 +10,7 @@ import androidx.room.PrimaryKey;
  */
 @Entity(tableName = "word_table")
 public class Word {
-    @PrimaryKey(autoGenerate = true)
+    @PrimaryKey
     @NonNull
     @ColumnInfo(name = "word")
     private String mWord;
