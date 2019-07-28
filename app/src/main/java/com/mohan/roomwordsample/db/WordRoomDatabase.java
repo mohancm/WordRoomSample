e0fb9fb047834cbc4e1389bd37257eaac65e2719
Mohan Cm <mohanmanjappa@gmail.com>
2019-07-28 23:16:51 +0530

    Create Database with name as  "word_database"

--- app/src/main/java/com/mohan/roomwordsample/db/WordRoomDatabase.java
+++ app/src/main/java/com/mohan/roomwordsample/db/WordRoomDatabase.java
@@ -3,6 +3,7 @@ package com.mohan.roomwordsample.db;
 import android.content.Context;
 
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.mohan.roomwordsample.Model.Word;
@@ -21,6 +22,9 @@ public abstract class WordRoomDatabase extends RoomDatabase {
             synchronized (WordRoomDatabase.class) {
                 if (INSTANCE == null) {
                     // Create database here
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "word_database")
+                            .build();
                 }
             }
         }
