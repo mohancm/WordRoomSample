c009593cff45cadf4a731ccd80ea597b290819dd
Mohan Cm <mohanmanjappa@gmail.com>
2019-07-28 22:48:41 +0530

    Implement room database annotations to make it meaningful

--- app/src/main/java/com/mohan/roomwordsample/Model/Word.java
+++ app/src/main/java/com/mohan/roomwordsample/Model/Word.java
@@ -1,12 +1,18 @@
 package com.mohan.roomwordsample.Model;
 
 import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 /**
  * Created by @mohancm on 28-07-2019 09:14 PM.
  */
+@Entity(tableName = "word_table")
 public class Word {
-
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "word")
     private String mWord;
 
     public Word(@NonNull String word) {
