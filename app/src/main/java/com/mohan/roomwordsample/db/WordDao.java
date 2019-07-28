11ae9add119061c29ed1b41690700a6edd1d5f4d
Mohan Cm <mohanmanjappa@gmail.com>
2019-07-28 22:54:27 +0530

    Create WordDao * Annotate the class with @Dao(Data Access Object) required by Room Database * Implement Dao methods and annotates it.

---
+++ app/src/main/java/com/mohan/roomwordsample/db/WordDao.java
@@ -0,0 +1,26 @@
+package com.mohan.roomwordsample.db;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.mohan.roomwordsample.Model.Word;
+
+import java.util.List;
+
+/**
+ * Created by @mohancm on 28-07-2019 10:51 PM.
+ */
+@Dao
+public interface WordDao {
+
+    @Insert
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    List<Word> getAllWords();
+
+}
