462f611b97f7618f49b7362b9f2804dda3d363da
Mohan Cm <mohanmanjappa@gmail.com>
2019-07-29 23:23:09 +0530

    Populate the database

--- app/src/main/java/com/mohan/roomwordsample/db/WordRoomDatabase.java
+++ app/src/main/java/com/mohan/roomwordsample/db/WordRoomDatabase.java
@@ -1,10 +1,13 @@
 package com.mohan.roomwordsample.db;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.mohan.roomwordsample.Model.Word;
 
@@ -24,10 +27,42 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                     // Create database here
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             WordRoomDatabase.class, "word_database")
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db){
+            super.onOpen(db);
+            new PopulateDbAsync(INSTANCE).execute();
+        }
+    };
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void>{
+
+        private final WordDao mDao;
+
+        PopulateDbAsync(WordRoomDatabase db){
+            mDao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... parmas) {
+            mDao.deleteAll();
+            Word word = new Word("Hello");
+            mDao.insert(word);
+            word = new Word("World");
+            mDao.insert(word);
+            word = new Word("Sup");
+            mDao.insert(word);
+            word = new Word("Mohan Cm");
+            mDao.insert(word);
+            return null;
+        }
     }
 }
